0f4539ee75e490c0efd04c218043029dae511e96
Max Hoffmann <tsov@me.com>
2015-03-30 16:37:22 +0200

    Added session stats model

---
+++ lib/transmission/model/session_stats.rb
@@ -0,0 +1,25 @@
+module Transmission
+  module Model
+    class SessionStats
+
+      attr_accessor :attributes, :connector
+
+      def initialize(session_object, connector)
+        @attributes = session_object
+        @connector = connector
+      end
+
+      class << self
+        def get(options = {})
+          rpc = options[:connector] || connector
+          body = rpc.get_session_stats options
+          SessionStats.new body, rpc
+        end
+
+        def connector
+          Transmission::Config.get_connector
+        end
+      end
+    end
+  end
+end
